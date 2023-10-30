831a1a39b6a63b0bf87119140dfd70ee0b5919b0
RandomCad <mmont4850@gmail.com>
2023-10-30 08:28:25 +0100

    Not working first chang to not change the formating

--- RichTextNumbering/RichTextNumbering/MainWindow.xaml.cs
+++ RichTextNumbering/RichTextNumbering/MainWindow.xaml.cs
@@ -26,9 +26,11 @@ namespace RichTextNumbering
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Get the text from the RichTextBox
-            string text = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+        {// Get the text from the RichTextBox
+            TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+
+            // Get the original text with formatting
+            string text = textRange.Text;
 
             // Split the text into lines
             string[] lines = text.Split('\n');
@@ -36,17 +38,37 @@ namespace RichTextNumbering
             // Create a new Paragraph for the RichTextBox
             Paragraph paragraph = new Paragraph();
 
-            // Add line numbers to each line and append to the Paragraph
+            // Loop through lines and add line numbers with preserved formatting
             for (int i = 0; i < lines.Length; i++)
             {
-                paragraph.Inlines.Add(new Run((i + 1).ToString() + ": ")); // Add line number
-                paragraph.Inlines.Add(new Run(lines[i])); // Add the line's text
+                // Create a new Run with the original formatting
+                Run run = new Run(lines[i]);
+
+                // Copy the formatting from the original text to the new Run
+                if (i < textRange.Text.Length)
+                {
+                    TextPointer start = textRange.Start.GetPositionAtOffset(lines[i].Length, (LogicalDirection)i);
+                    TextPointer end = start.GetPositionAtOffset(lines[i].Length, (LogicalDirection)1);
+                    TextRange lineRange = new TextRange(start, end);
+                    run.TextDecorations = lineRange.TextDecorations;
+                    run.FontFamily = lineRange.GetPropertyValue(TextElement.FontFamilyProperty) as FontFamily;
+                    run.FontSize = (double)lineRange.GetPropertyValue(TextElement.FontSizeProperty);
+                    run.FontWeight = (FontWeight)lineRange.GetPropertyValue(TextElement.FontWeightProperty);
+                    run.FontStyle = (FontStyle)lineRange.GetPropertyValue(TextElement.FontStyleProperty);
+                    run.Foreground = lineRange.GetPropertyValue(TextElement.ForegroundProperty) as Brush;
+                }
+
+                // Add line number
+                paragraph.Inlines.Add(new Run((i + 1).ToString() + ": "));
+
+                // Add the line's text with original formatting
+                paragraph.Inlines.Add(run);
             }
 
             // Clear the existing content in the RichTextBox
             richTextBox.Document.Blocks.Clear();
 
-            // Add the new paragraph with line numbers to the RichTextBox
+            // Add the new paragraph with line numbers and preserved formatting to the RichTextBox
             richTextBox.Document.Blocks.Add(paragraph);
         }
     }
