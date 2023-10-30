4b7d8c39d33837ccb0f6ec0e5e32d06cb36761cb
RandomCad <mmont4850@gmail.com>
2023-10-30 08:33:55 +0100

    Runnabel try with ocuring errors

--- RichTextNumbering/RichTextNumbering/MainWindow.xaml.cs
+++ RichTextNumbering/RichTextNumbering/MainWindow.xaml.cs
@@ -28,55 +28,47 @@ namespace RichTextNumbering
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             // Get the text from the RichTextBox
-            TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+            FlowDocument doc = richTextBox.Document;
 
-            // Get the original text with formatting
-            string text = textRange.Text;
-
-            // Split the text into lines
-            string[] lines = text.Split('\n');
+            // Create a new FlowDocument for the RichTextBox
+            FlowDocument newDoc = new FlowDocument();
 
             // Create a new Paragraph for the RichTextBox
             Paragraph paragraph = new Paragraph();
 
-            // Loop through lines and add line numbers with preserved formatting
-            for (int i = 0; i < lines.Length; i++)
+            int lineNumber = 1;
+
+            foreach (Block block in doc.Blocks)
             {
-                // Create a new Run with the original formatting
-                Run run = new Run(lines[i]);
-
-                // Copy the formatting from the original text to the new Run
-                if (i < textRange.Text.Length)
+                if (block is Paragraph originalParagraph)
                 {
-                    TextPointer start = textRange.Start.GetPositionAtOffset(lines[i].Length, (LogicalDirection)i);
-                    TextPointer end = start.GetPositionAtOffset(lines[i].Length, (LogicalDirection)1);
-                    TextRange lineRange = new TextRange(start, end);
+                    // Create a new paragraph for each original paragraph
+                    Paragraph newParagraph = new Paragraph();
 
-                    // Copy TextDecorations individually
-                    foreach (TextDecoration decoration in lineRange.TextDecorations)
-                    {
-                        run.TextDecorations.Add(decoration);
-                    }
+                    // Copy the original paragraph's properties
+                    newParagraph.TextAlignment = originalParagraph.TextAlignment;
+                    newParagraph.FontSize = originalParagraph.FontSize;
+                    newParagraph.FontFamily = originalParagraph.FontFamily;
+                    newParagraph.FontWeight = originalParagraph.FontWeight;
+                    newParagraph.FontStyle = originalParagraph.FontStyle;
+                    newParagraph.Foreground = originalParagraph.Foreground;
 
-                    run.FontFamily = lineRange.GetPropertyValue(TextElement.FontFamilyProperty) as FontFamily;
-                    run.FontSize = (double)lineRange.GetPropertyValue(TextElement.FontSizeProperty);
-                    run.FontWeight = (FontWeight)lineRange.GetPropertyValue(TextElement.FontWeightProperty);
-                    run.FontStyle = (FontStyle)lineRange.GetPropertyValue(TextElement.FontStyleProperty);
-                    run.Foreground = lineRange.GetPropertyValue(TextElement.ForegroundProperty) as Brush;
+                    // Add the line number and original text to the new paragraph
+                    newParagraph.Inlines.Add(new Run(lineNumber + ": "));
+                    newParagraph.Inlines.AddRange(originalParagraph.Inlines);
+
+                    // Add the new paragraph to the new document
+                    newDoc.Blocks.Add(newParagraph);
+
+                    lineNumber++;
                 }
-
-                // Add line number
-                paragraph.Inlines.Add(new Run((i + 1).ToString() + ": "));
-
-                // Add the line's text with original formatting
-                paragraph.Inlines.Add(run);
             }
 
             // Clear the existing content in the RichTextBox
             richTextBox.Document.Blocks.Clear();
 
-            // Add the new paragraph with line numbers and preserved formatting to the RichTextBox
-            richTextBox.Document.Blocks.Add(paragraph);
+            // Add the new FlowDocument with line numbers and preserved formatting to the RichTextBox
+            richTextBox.Document = newDoc;
         }
     }
 }
