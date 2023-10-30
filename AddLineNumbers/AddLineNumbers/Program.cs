787d6fb9197b5424aef48f770e18fa36d1505230
RandomCad <mmont4850@gmail.com>
2023-10-30 09:04:42 +0100

    Working docx editing aproche

---
+++ AddLineNumbers/AddLineNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Load the existing DOCX document
+using Xceed.Words.NET;
+
+DocX doc = DocX.Load("input.docx");
+
+int lineNumber = 1;
+
+// Iterate through each paragraph in the document and add line numbers
+foreach (var paragraph in doc.Paragraphs)
+{
+    paragraph.InsertText(0, $"{lineNumber}. ");
+    lineNumber++;
+}
+
+// Save the modified document to a new file
+doc.SaveAs("output.docx");
