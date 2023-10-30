abcc579f54c3f0111e31d21187da730a436f250e
RandomCad <mmont4850@gmail.com>
2023-10-30 09:11:42 +0100

    Not corectly working self changed

--- AddLineNumbers/AddLineNumbers/Program.cs
+++ AddLineNumbers/AddLineNumbers/Program.cs
@@ -3,23 +3,30 @@
 using System.Drawing;
 using Xceed.Words.NET;
 
-// Load the existing DOCX document
-DocX doc = DocX.Load("input.docx");
-
-int lineNumber = 1;
-
-// Iterate through each paragraph in the document and add line numbers
-foreach (var paragraph in doc.Paragraphs)
+internal class Program
 {
-    // Insert the line number at the beginning of the paragraph
-    var run = paragraph.InsertParagraph(0).Append($"{lineNumber}. ");
+    private static void Main(string[] args)
+    {
+        // Load the existing DOCX document
+        DocX doc = DocX.Load("input.docx");
 
-    // Set the formatting for the line numbers (e.g., black color)
-    run.Color(Color.Black);
-    run.Bold(); // You can also set other formatting options as needed
+        int lineNumber = 1;
 
-    lineNumber++;
+        // Iterate through each paragraph in the document and add line numbers
+        for(int i = 0; i< doc.Paragraphs.Count; i+=2)
+        {
+            var paragraph = doc.Paragraphs[i];
+            // Insert the line number at the beginning of the paragraph
+            var run = paragraph.InsertParagraphAfterSelf(paragraph).Append($"{lineNumber}. ");
+
+            // Set the formatting for the line numbers (e.g., black color)
+            run.Color(Color.Black);
+            run.Bold(); // You can also set other formatting options as needed
+
+            lineNumber++;
+        }
+
+        // Save the modified document to a new file
+        doc.SaveAs("output.docx");
+    }
 }
-
-// Save the modified document to a new file
-doc.SaveAs("output.docx");
