70ca92754ddce3b3d37766d062b149ede72be734
RandomCad <mmont4850@gmail.com>
2023-10-30 09:06:46 +0100

    Not working were numbers are diverent collore

--- AddLineNumbers/AddLineNumbers/Program.cs
+++ AddLineNumbers/AddLineNumbers/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Load the existing DOCX document
+using System.Drawing;
 using Xceed.Words.NET;
 
+// Load the existing DOCX document
 DocX doc = DocX.Load("input.docx");
 
 int lineNumber = 1;
@@ -9,7 +11,13 @@ int lineNumber = 1;
 // Iterate through each paragraph in the document and add line numbers
 foreach (var paragraph in doc.Paragraphs)
 {
-    paragraph.InsertText(0, $"{lineNumber}. ");
+    // Insert the line number at the beginning of the paragraph
+    var run = paragraph.InsertParagraph(0).Append($"{lineNumber}. ");
+
+    // Set the formatting for the line numbers (e.g., black color)
+    run.Color(Color.Black);
+    run.Bold(); // You can also set other formatting options as needed
+
     lineNumber++;
 }
 
